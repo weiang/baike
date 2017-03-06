17911cb7e5fb1d2335b15f58fcf21ea60caa9908
weiang <weiang123@gmail.com>
2017-03-06 15:17:49 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python
 # -*- encoding=utf-8 -*-
 
+import os
+import sys
+
 from baike import *
 
-files = ["test.html", "test2.html"]
+def load_files(dir):
+    r = []
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            file_path = os.path.join(root, f)
+            r.append(file_path)
 
-init_nlp_env()
+        for d in dirs:
+            dir_path = os.path.join(root, d)
+            r.extend(load_files(dir_path))
+    return r 
 
-for f in files:
-    p = BaikePerson(f)
-    s = p.to_json() 
-    print s
+def main():
+    if len(sys.argv) != 2:
+        print "Usage: %s <dir_name>" % (sys.argv[0])
+        sys.exit(1)
 
-destroy_nlp_env()
+    dir_name = sys.argv[1]
+    files = load_files(dir_name)
+
+    init_nlp_env()
+
+    for f in files:
+        p = BaikePerson(f)
+        s = p.to_json() 
+        print s
+
+    destroy_nlp_env()
+
+if __name__ == '__main__':
+    main()
